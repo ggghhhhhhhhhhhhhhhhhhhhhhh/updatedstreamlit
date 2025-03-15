920838c3c2ffcc29c6bab3be2e5be60ed3e15f05
lucky <87254791+ggghhhhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2025-03-15 22:22:25 +0530

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,28 +1,25 @@
-import streamlit as st 
-from models import User 
-import os 
-from sqlalchemy.orm import sessionmaker 
-from sqlalchemy import create_engine 
 
-os.makedirs('instance',exist_ok=True)
-engine=create_engine('sqlite:///instance/recoverease.db',connect_args={"check_same_thread":False})
-SessionLocal=sessionmaker(bind=engine)
+import streamlit as st
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from models import User
+
+# Initialize database connection
+engine = create_engine('sqlite:///instance/recoverease.db')
+SessionLocal = sessionmaker(bind=engine)
 
 st.title("Login to RecoverEase")
 
-username=st.text_input("Username")
-password=st.text_input("Password",type="password")
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
 
 if st.button("Login"):
-  s=SessionLocal()
-  user=s.query(User).filter_by(username=username,password=password).first()
-  if user:
-      s.close()
-      st.session_state.logged_in=True 
-      st.session_state.current_user=username 
-      st.experimental_set_query_params(page="home")
-      st.stop()  
-  else:
-      s.close()
-      st.error("Invalid username or password.")
+    session = SessionLocal()
+    user = session.query(User).filter_by(username=username, password=password).first()
+    if user:
+        st.success(f"Welcome back, {username}!")
+    else:
+        st.error("Invalid username or password.")
+    session.close()
 
+st.markdown("[Don't have an account? Register here](./2_Register)")
