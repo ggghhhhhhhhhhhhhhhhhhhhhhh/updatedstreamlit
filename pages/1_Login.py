b7c7f40be519d0d9c4801e06775a7e83ec02dbd9
lucky <87254791+ggghhhhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2025-03-15 21:57:46 +0530

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,30 +1,28 @@
-import streamlit as st
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from models import User
+import streamlit as st 
+from models import User 
+import os 
+from sqlalchemy.orm import sessionmaker 
+from sqlalchemy import create_engine 
 
-# Initialize database connection
-engine = create_engine('sqlite:///instance/recoverease.db')
-SessionLocal = sessionmaker(bind=engine)
+os.makedirs('instance',exist_ok=True)
+engine=create_engine('sqlite:///instance/recoverease.db',connect_args={"check_same_thread":False})
+SessionLocal=sessionmaker(bind=engine)
 
 st.title("Login to RecoverEase")
 
-# Input fields for login
-username = st.text_input("Username")
-password = st.text_input("Password", type="password")
+username=st.text_input("Username")
+password=st.text_input("Password",type="password")
 
 if st.button("Login"):
-    session = SessionLocal()
-    user = session.query(User).filter_by(username=username, password=password).first()
-    if user:
-        # Set session state to logged in and store user information
-        st.session_state.logged_in = True
-        st.session_state.current_user = username
-        st.success(f"Welcome back, {username}!")
-        st.experimental_set_query_params(page="home")  # Redirect to home page after login
-        st.stop()
-    else:
-        st.error("Invalid username or password.")
-    session.close()
+  s=SessionLocal()
+  user=s.query(User).filter_by(username=username,password=password).first()
+  if user:
+      s.close()
+      st.session_state.logged_in=True 
+      st.session_state.current_user=username 
+      st.experimental_set_query_params(page="home")
+      st.stop()  
+  else:
+      s.close()
+      st.error("Invalid username or password.")
 
-st.markdown("[Don't have an account? Register here](./2_Register)")
