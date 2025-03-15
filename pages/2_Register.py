5dac97ff9e8cf9791015403fd23efa0ea7b49e94
lucky <87254791+ggghhhhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2025-03-15 21:58:12 +0530

    Update 2_Register.py

--- pages/2_Register.py
+++ pages/2_Register.py
@@ -1,37 +1,30 @@
 import streamlit as st
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from models import User
+import os 
+from sqlalchemy.orm import sessionmaker 
+from sqlalchemy import create_engine 
 
-# Initialize database connection
-engine = create_engine('sqlite:///instance/recoverease.db')
-SessionLocal = sessionmaker(bind=engine)
+os.makedirs('instance', exist_ok=True)
+engine=create_engine('sqlite:///instance/recoverease.db',connect_args={"check_same_thread":False})
+SessionLocal=sessionmaker(bind=engine)
 
-st.title("Register for RecoverEase")
+st.title("Register to RecoverEase")
 
-# Input fields for registration
-username = st.text_input("Username")
-email = st.text_input("Email")
-password = st.text_input("Password", type="password")
-confirm_password = st.text_input("Confirm Password", type="password")
+username=st.text_input("Username")
+email=st.text_input("Email")
+password=st.text_input("Password",type="password")
 
 if st.button("Register"):
-    if password != confirm_password:
-        st.error("Passwords do not match!")
-    else:
-        session = SessionLocal()
-        existing_user = session.query(User).filter_by(username=username).first()
-        if existing_user:
-            st.error("Username already exists. Please choose a different username.")
-        else:
-            new_user = User(username=username, email=email, password=password)
-            session.add(new_user)
-            session.commit()
-            st.success(f"Account created successfully for {username}!")
-            # Automatically log in after registration
-            st.session_state.logged_in = True
-            st.session_state.current_user = username
-            st.experimental_set_query_params(page="home")  # Redirect to home page
-        session.close()
-
-st.markdown("[Already have an account? Login here](./1_Login)")
+  with SessionLocal() as s:
+      existing=s.query(User).filter_by(username=username).first()
+      if existing_user:
+          st.error("Username already exists.")
+      else:
+          user_new=User(username=username,email=email,password=password)
+          s.add(user_new)
+          s.commit()
+          s.close()
+          st.success(f"Account created successfully for {username}!")
+          st.session_state.logged_in=True 
+          st.session_state.current_user=username 
+          st.experimental_set_query_params(page="home")
