d33a10a173ad6284b8452be2f8f8090e0f6b22d1
lucky <87254791+ggghhhhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2025-03-15 21:58:44 +0530

    Update 3_Home.py

--- pages/3_Home.py
+++ pages/3_Home.py
@@ -1,28 +1,33 @@
 import streamlit as st
+import pandas as pd
+from models import LostItem
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from models import LostItem
 
-engine = create_engine('sqlite:///instance/recoverease.db')
+os.makedirs('instance', exist_ok=True)
+engine = create_engine('sqlite:///instance/recoverease.db', connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 
 if not st.session_state.get("logged_in", False):
     st.warning("Please log in to access this page.")
 else:
     if st.sidebar.button("Logout"):
-        st.session_state.logged_in = False
+        st.session_state.logged_in=False
 
-st.title(f"Home - Lost Items ({st.session_state.current_user})")
-
-session = SessionLocal()
-lost_items = session.query(LostItem).all()
-
-if lost_items:
-    table_data = [{"Owner Name": item.owner_name,
-                   "Description": item.item_desc,
-                   "Last Seen Location": item.last_seen_location,
-                   "Status": item.status} for item in lost_items]
+    st.title(f"Home - Lost Items ({st.session_state.current_user})")
     
-import pandas as pd
-
-df_table_data=pd.DataFrame(table
+    session=SessionLocal()
+    
+    items=session.query(LostItem).all()
+    
+    if items:
+        df=pd.DataFrame([{
+            "Owner Name":item.owner_name,
+            "Description":item.item_desc,
+            "Last Seen Location":item.last_seen_location,
+            "Status":item.status} for item in items])
+        
+        st.dataframe(df_table_data)
+    
+session.close()
